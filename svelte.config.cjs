c0f77d399348e917fe2516c004a4393bf1edbd51
Karol Moroz <k.j.moroz@gmail.com>
2021-05-16 23:43:26 +0800

    Install application dependencies from old repo

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,7 +1,15 @@
-const sveltePreprocess = require('svelte-preprocess')
+const sveltePreprocess = require("svelte-preprocess");
+
+const preprocessOptions = {
+  sourceMap: true, // "you would always want sourcemaps for the IDE" – dummdidumm
+  defaults: {
+    script: "typescript",
+    style: "scss"
+  }
+};
 
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: sveltePreprocess(preprocessOptions)
+};
